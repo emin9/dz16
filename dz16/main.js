c2b25ab9832f79edbe4b6ef7dcdd4386218ca6a0
emin9 <129058356+emin9@users.noreply.github.com>
2023-05-26 15:54:30 +0300

    Add files via upload

---
+++ dz16/main.js
@@ -0,0 +1,39 @@
+let num = 2
+
+function num2(){
+ if(num < 10){
+    console.log("константа меньше 10")
+ }
+ else{
+    console.log("константа не меньше 10")
+ }
+}
+console.log(num2())
+
+function al(){
+    alert("привет мир")
+    
+}
+al()
+
+function js(){
+    console.log('Javascript')
+}
+console.log(js());
+
+
+
+let text = 'nelorem'
+
+
+function text2(){
+    if (text == 'lorem'){
+        console.log('У вас ошибка в тексте')
+    }
+    else{
+      console.log(text)
+
+    }
+  
+}
+console.log(text2())
